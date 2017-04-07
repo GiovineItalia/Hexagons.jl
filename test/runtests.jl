34f4a62d50ac1d784d3e423fe4e968593059d2c2
Ben Arthur <arthurb@hhmi.org>
2017-04-07 11:55:31 -0400

    added rudimentary tests

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using Hexagons
+using Base.Test
+
+convert(HexagonOffsetOddR, HexagonAxial(2, 4))
+
+x, y = center(HexagonAxial(2, 3))
+@test isapprox(x,7.06217782649107)
+@test isapprox(y,5.5)
+
+h = cube_round(23.5, 4.67)
+
+collect(vertices(HexagonAxial(2, 3)))
